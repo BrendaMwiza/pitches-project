51aa582fa282f421e64d64a882c69c0d751bdc4a
BrendaMwiza <brendabrizy@gmail.com>
2019-09-20 16:19:01 +0200

    added templates files

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    category = Category.get_category()
+    return render_template('index.html', category = category)
+    
