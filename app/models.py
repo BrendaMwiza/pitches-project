50ff5c8492d7f29e8aeae745c366809e882ab25b
BrendaMwiza <brendabrizy@gmail.com>
2019-09-22 21:21:29 +0200

    added a comment class

--- app/models.py
+++ app/models.py
@@ -53,13 +53,12 @@ class Pitch(db.Model):
     def clear_pitches(cls):
         Pitch.all_pitches.clear()
 
-    # display pitches
 
     def get_pitches(id):
         pitches = Pitch.query.filter_by(category_id=id).all()
         return pitches
 
-
+# Category class
 class Category(db.Model):
     __tablename__ = 'categories'
     id = db.Column(db.Integer, primary_key = True)
@@ -73,26 +72,26 @@ class Category(db.Model):
     def get_categories(cls):
         categories = Category.query.all()
         return categories
+# Comment class
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key = True)
+    feedback = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
+    votes = db.relationship('Vote', backref = 'comments', lazy = "dynamic")
 
-# class Comment(db.Model):
-#     __tablename__ = 'comments'
-#     id = db.Column(db.Integer, primary_key = True)
-#     feedback = db.Column(db.String)
-#     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-#     pitch_id = db.Column(db.Integer, db.ForeignKey('pitches.id'))
-#     votes = db.relationship('Vote', backref = 'comments', lazy = "dynamic")
+    def save_comment(self):
+        '''
+        Function that saves comments
+        '''
+        db.session.add(self)
+        db.session.commit()
 
-#     def save_comment(self):
-#         '''
-#         Function that saves comments
-#         '''
-#         db.session.add(self)
-#         db.session.commit()
-
-#     @classmethod
-#     def get_comments(self, id):
-#         comment = Comments.query.order_by(Comments.time_posted.desc()).filter_by(pitches_id=id).all()
-#         return comment
+    @classmethod
+    def get_comments(self, id):
+        comment = Comments.query.order_by(Comments.time_posted.desc()).filter_by(pitches_id=id).all()
+        return comment
 
 
 # class Vote(db.Model):
