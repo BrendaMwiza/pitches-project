bfc8cc57d392bbbdeaa98636e4e6178e4de33aa2
BrendaMwiza <brendabrizy@gmail.com>
2019-09-22 17:55:34 +0200

    removed bugs

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
-from app import create_app, db
+from app import kora_app, db
 from flask_script import Manager, Server
-# from app.models import User, Pitch, Category, Vote, Comment
-# from flask_migrate import Migrate, MigrateCommand
+from app.models import User
+# , Pitch, Category, Vote, Comment
+from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
 app = kora_app('production')
@@ -10,7 +11,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 manager.add_command('server',Server)
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def test():
