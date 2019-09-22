fda41ec8bacca38ea8f08d58122a3e8a06c21679
BrendaMwiza <brendabrizy@gmail.com>
2019-09-22 20:49:09 +0200

    register the blueprint instance

--- app/__init__.py
+++ app/__init__.py
@@ -25,14 +25,14 @@ def kora_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
-#     login_manager.init_app(app)
+    login_manager.init_app(app)
 
     # Registering the blueprint
     from .main import main as main_blueprint
-    # app.register_blueprint(main_blueprint)
+    app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    # app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
+    app.register_blueprint(auth_blueprint, url_prefix = '/authenticate')
 
     # configure UploadSet
     configure_uploads(app,photos)
