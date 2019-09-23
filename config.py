018bea1fe902b5ab6d6bcf44f82cb609f0054cdd
BrendaMwiza <brendabrizy@gmail.com>
2019-09-23 14:31:14 +0200

    imported photo instance

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('isimbi')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
+export SECRET_KEY = 'isimbi'
+export MAIL_USERNAME = 'brendabrizy@gmail.com'
+export MAIL_PASSWORD = 'isimbiS2019'
 
 python3.6 manage.py server
